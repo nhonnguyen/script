dea50f5469bb34b59b631ba7aa2fe3cfc44f2efa
nhon nguyen <nhontrnguyen@gmail.com>
2018-05-16 15:32:48 +0700

    edit script

--- log.sh
+++ log.sh
@@ -2,5 +2,5 @@
 FILE_NAME="/root/log_"`date +%Y_%m_%d`".log"
 DATE_NOW=`date +%Y/%m/%d-%H:%M:%S`
 echo "Creating txt file"
-echo "Created by Nguyen Huu Nhon. Created time is $DATE_NOW" >  $FILE_NAME
+echo "Created by Nguyen Huu Nhon 1. Created time is $DATE_NOW" >  $FILE_NAME
 echo "Completed"
